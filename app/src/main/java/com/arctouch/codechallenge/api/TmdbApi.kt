b433a3ea60dc14d3cf347e0a643ba8b41a9218c4
Caio Melo <caio.melo@kanamobi.com.br>
2020-02-10 13:40:48 -0300

    Implementing TmdbApp class / implementing Dependency Injection with Koin

--- app/src/main/java/com/arctouch/codechallenge/api/TmdbApi.kt
+++ app/src/main/java/com/arctouch/codechallenge/api/TmdbApi.kt
@@ -10,31 +10,37 @@ import retrofit2.http.Query
 
 interface TmdbApi {
 
-    companion object {
-        const val URL = "https://api.themoviedb.org/3/"
-        const val API_KEY = "1f54bd990f1cdfb230adb312546d765d"
-        const val DEFAULT_LANGUAGE = "pt-BR"
-        const val DEFAULT_REGION = "BR"
-    }
-
     @GET("genre/movie/list")
     fun genres(
-        @Query("api_key") apiKey: String,
-        @Query("language") language: String
+            @Query("api_key") apiKey: String? = API_KEY,
+            @Query("language") language: String? = DEFAULT_LANGUAGE
     ): Observable<GenreResponse>
 
     @GET("movie/upcoming")
     fun upcomingMovies(
-        @Query("api_key") apiKey: String,
-        @Query("language") language: String,
-        @Query("page") page: Long,
-        @Query("region") region: String
+            @Query("api_key") apiKey: String? = API_KEY,
+            @Query("language") language: String? = DEFAULT_LANGUAGE,
+            @Query("page") page: Long,
+            @Query("region") region: String? = DEFAULT_REGION
     ): Observable<UpcomingMoviesResponse>
 
     @GET("movie/{id}")
     fun movie(
-        @Path("id") id: Long,
-        @Query("api_key") apiKey: String,
-        @Query("language") language: String
+            @Path("id") id: Long,
+            @Query("api_key") apiKey: String? = API_KEY,
+            @Query("language") language: String? = DEFAULT_LANGUAGE
     ): Observable<Movie>
+
+    companion object {
+        private const val POSTER_URL = "https://image.tmdb.org/t/p/w154"
+        private const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
+        const val URL = "https://api.themoviedb.org/3/"
+        const val API_KEY = "1f54bd990f1cdfb230adb312546d765d"
+        const val DEFAULT_LANGUAGE = "pt-BR"
+        const val DEFAULT_REGION = "BR"
+
+        fun buildPosterUrl(posterPath: String): String = "$POSTER_URL$posterPath?api_key=$API_KEY"
+
+        fun buildBackdropUrl(backdropPath: String): String = "$BACKDROP_URL$backdropPath?api_key=$API_KEY"
+    }
 }
